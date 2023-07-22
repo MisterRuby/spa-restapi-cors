47567e44cf3b23aadd4a7b29ed63345d6def0906
MisterRuby <ruby8700@naver.com>
2023-07-22 15:35:05 +0900

    add : API 요청 페이지 추가

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import axios from "axios";
+
+const onclickButton = () => {
+  alert("API 요청!")
+  axios.post('http://localhost:8080')
+    .then(res => {
+      alert("API 요청 완료!")
+    })
+    .catch(e => {
+      alert("API 요청 실패!")
+    })
+}
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <div>Web Server URL : localhost:5173</div>
+    <div>API Server URL : localhost:8080</div>
+    <button @click="onclickButton">API 요청</button>
+  </div>
 </template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+button {
+  border : none;
+  background-color: deepskyblue;
+  color: white;
+  width: 100px;
+  height: 30px;
+  font-size: 16px;
+}
+</style>
